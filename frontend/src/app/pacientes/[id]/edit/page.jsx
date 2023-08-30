bed90b7fed27eaec0111da9b07fdae2b9e1b433b
Nicolás Bobb <nicobobb@gmail.com>
2023-08-30 00:38:58 -0300

    Update page.jsx

--- frontend/src/app/pacientes/[id]/edit/page.jsx
+++ frontend/src/app/pacientes/[id]/edit/page.jsx
@@ -1,5 +1,22 @@
+"use client";
+
 const Edit = ({ params }) => {
-  return <div>{params.id}</div>;
+  const handleDelete = async () => {
+    console.log("Hola Mundo");
+    const res = await fetch(
+      `http://localhost:4000/api/v1/pacientes/${params.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const data = await res.json();
+  };
+  return (
+    <div>
+      {params.id}
+      <button onClick={handleDelete}>Borrar</button>
+    </div>
+  );
 };
 
 export default Edit;
