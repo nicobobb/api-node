9291779565c90b7da924602e86814f934eba8704
Nicolás Bobb <nicobobb@gmail.com>
2023-09-12 09:27:06 -0300

    middlewares

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const routerApiV1 = require('./v1/routes');
 const routerApiV2 = require('./v2/routes');
 const app = express();
 const port = 4000;
+const { logErrors, errorHandler } = require('./middlewares/error');
 
 // REQuest >> peticion
 // RESponse >> respuesta
@@ -30,3 +31,14 @@ app.listen(port, () => {
 
 routerApiV1(app);
 routerApiV2(app);
+
+// Middlewares (siempre van despues de las rutas)
+// https://expressjs.com/es/guide/using-middleware.html
+/*
+ Sirve para:
+  1) Validar Datos
+  2) Capturar Errores
+  3) Permisos y validaciones, etc
+ */
+app.use(logErrors);
+app.use(errorHandler);
